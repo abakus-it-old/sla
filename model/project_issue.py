422fd3bbb862314afe58b09933c6aa6782f773d6
Bernard Delhez <bernarddelhez@gmail.com>
2015-04-01 12:48:06 +0200

    Update project_issue.py

--- model/project_issue.py
+++ model/project_issue.py
@@ -8,7 +8,7 @@ class project_issue_sla(models.Model):
 
     #priority_sla = fields.Many2one(comodel_name='project.issue.priority', string="Issue's priority")
     sla_rule_ids = fields.One2many(comodel_name='project.sla.rule',inverse_name='sla_id', related='analytic_account_id.contract_type.sla_id.sla_rule_ids', string="SLA rules", store=False)
-    sla_status = fields.Char(related='analytic_account_id.sla_status', string="SLA", store=False)
+    sla_bool = fields.Boolean(related='analytic_account_id.sla_bool', string="SLA", store=False)
     
     def get_date_difference_with_working_time_in_minutes(self, cr, uid, create_date, check_date):
         resource_calendar_obj = self.pool.get('resource.calendar')
