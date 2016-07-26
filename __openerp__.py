15909fbf6c623ad43afca1b0f551b57d78b46f42
Valentin Thirion <valentinthirion@gmail.com>
2016-07-26 16:10:58 +0200

    Ported SLA, rules and actions to 9.0

--- __openerp__.py
+++ __openerp__.py
@@ -1,11 +1,16 @@
 ﻿{
-    'name': "AbAKUS SLA",
+    'name': "AbAKUS Project SLA",
     'version': '1.0',
-    'depends': ['account_analytic_analysis',],#'contract_report'
+    'depends': [
+        'project',
+        'analytic',
+    ],#'contract_report'
     'author': "Bernard DELHEZ, AbAKUS it-solutions SARL",
     'website': "http://www.abakusitsolutions.eu",
     'category': 'Contract',
-    'description': """This modules adds the SLA management for AbAKUS.
+    'description': """Service Level Agreement Management for Project Issues
+
+This modules adds the SLA management for AbAKUS.
     - It adds in contract type a SLA field.
     - You can select many rules in SLA.
     - For one rule, you can select many action. This actions are Python code that saved in database.
@@ -16,15 +21,19 @@
     SLA Cron is also corrected because it needs to be taken into account for OS and SD which does not have the same SLA data.
     
 This module has been developed by Bernard Delhez, intern @ AbAKUS it-solutions, under the control of Valentin Thirion.""",
-    'data': ['view/account_analytic_account_view.xml',
-             'view/account_analytic_account_type_view.xml',
-             'view/project_sla_view.xml',
-             'view/project_sla_rule_view.xml',
-             'view/project_issue_view.xml',
-             #'view/project_issue_priority_view.xml',
-             'view/project_sla_action_view.xml',
-             'project_sla_cron.xml',
-             'project_sla_action_data.xml',
-             'security/ir.model.access.csv',
+    'data': [
+        'view/project_sla_view.xml',
+        'view/project_sla_rule_view.xml',
+        'view/project_sla_action_view.xml',
+        'project_sla_action_data.xml',
+        #'view/account_analytic_account_view.xml',
+        #'view/account_analytic_account_type_view.xml',
+
+        #'view/project_issue_view.xml',
+        ###'view/project_issue_priority_view.xml',
+
+        #'project_sla_cron.xml',
+
+        #'security/ir.model.access.csv',
     ],
 }
--- model/__init__.py
+++ model/__init__.py
@@ -1,7 +1,9 @@
-﻿from . import account_analytic_account
-from . import account_analytic_account_type
-from . import project_issue
-#from . import project_issue_priority
-from . import project_sla
-from . import project_sla_action
+﻿from . import project_sla
 from . import project_sla_rule
+from . import project_sla_action
+
+#from . import account_analytic_account
+#from . import account_analytic_account_type
+#from . import project_issue
+#from . import project_issue_priority
+
